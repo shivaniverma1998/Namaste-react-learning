a561c9e797b4c83e55d8e017c5464de6e63ec607
shivaniverma1606 <shivaniverma1606p@gmail.com>
2024-08-04 14:56:33 +0530

    this is all code of Lecture 1

---
+++ episode1/App.js
@@ -0,0 +1,23 @@
+// const heading = React.createElement(
+//     "h1",
+//      {id: "heading", xyz: "abc"},
+//       "Hello World from React program");
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [React.createElement(
+        "div",
+        {  id: "child1" },
+        [React.createElement("h1", {}, "i am h1 tag here"),React.createElement("h2", {}, "i am h2 tag here")]
+    ),
+    React.createElement(
+        "div",
+        {  id: "child2" },
+        [React.createElement("h1", {}, "i am h1 tag here"),React.createElement("h2", {}, "i am h2 tag here")]
+    )]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
